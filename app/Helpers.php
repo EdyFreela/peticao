d6da9a25ecd0e7af36c157f4fb68ebfe6b9b6be0
EdyFreela <31071891+EdyFreela@users.noreply.github.com>
2017-09-26 23:02:52 -0300

    Date Format Helpers
    25 Setembro, 2017

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,14 +1,33 @@
 <?php
 
 /**
-* change plain number to formatted currency
-*
+* change date format
+* 2017-09-25 09:00:59
+* to
+* 25 Setembro, 2017
 * @param $number
-* @param $currency
 */
 
 function formatDate($date)
-{
-   
-   return '----->'.$date;
+{ 
+	$year  = substr($date, 0, 4);
+	$month = substr($date, 5, 2); 
+	$day   = substr($date, 8, 2);
+
+	switch ($month) {
+	    case '01': $monthEx = 'Janeiro'; break;
+	    case '02': $monthEx = 'Fevereiro'; break;
+	    case '03': $monthEx = 'Março'; break;
+	    case '04': $monthEx = 'Abril'; break;
+	    case '05': $monthEx = 'Maio'; break;
+	    case '06': $monthEx = 'Junho'; break;
+	    case '07': $monthEx = 'Julho'; break;
+	    case '08': $monthEx = 'Agosto'; break;
+	    case '09': $monthEx = 'Setembro'; break;
+	    case '10': $monthEx = 'Outubro'; break;
+	    case '11': $monthEx = 'Novembro'; break;
+	    case '12': $monthEx = 'Dezembro'; break;
+	}
+
+	return $day.' '.$monthEx.', '.$year;
 }
