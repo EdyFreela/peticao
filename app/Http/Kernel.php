1b6b505296acfc6f87193430588740d0e340e0b1
EdyFreela <31071891+EdyFreela@users.noreply.github.com>
2017-10-17 18:36:51 -0300

    Force HTTPS
    Add in .htaccess
    
        # Force Https
        RewriteCond %{SERVER_PORT} 80
        RewriteRule ^(.*)$ https://campanhas.ipco.org.br/$1 [R,L]

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,6 +29,9 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+
+            // Force https in production
+            \App\Http\Middleware\HttpsProtocol::class            
         ],
 
         'api' => [
