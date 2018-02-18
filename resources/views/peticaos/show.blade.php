36d8b964cde649340e6ba00e158168a463c76326
EdyFreela <31071891+EdyFreela@users.noreply.github.com>
2018-02-18 19:02:58 -0300

    Script Correction On Scroll Stick Div

--- resources/views/peticaos/show.blade.php
+++ resources/views/peticaos/show.blade.php
@@ -382,6 +382,25 @@
 
 @section('script')
 <script>
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {myFunction()};
+
+    // Get the header
+    var header = document.getElementById("peticao-assinar");
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+</script>
+<script>
 $(document).ready(function() {
     $('.progress .progress-bar').css("width",
         function() {
